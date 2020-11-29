b5993ed6f354afd37e511e8fb26310b2432a511c
choH <henryzhongwork@hotmail.com>
2020-11-29 05:58:18 -0500

    🔨 Update quick test for extension (100/0/0 --> 19/81/6 --> 12/88/15).

--- demos/fgsm_quick_test.py
+++ demos/fgsm_quick_test.py
@@ -27,7 +27,7 @@ path = get_file('mnist_cnn_original.h5', extract=False, path=ART_DATA_PATH,
 classifier_model = load_model(path)
 classifier = KerasClassifier(clip_values=(min_, max_), model=classifier_model, use_logits=False)
 
-num = 1000
+num = 100
 classifier_model.summary()
 x_test_pred = np.argmax(classifier.predict(x_test[:num]), axis=1)
 nb_correct_pred = np.sum(x_test_pred == np.argmax(y_test[:num], axis=1))
@@ -50,18 +50,28 @@ print("Incorrectly classified: {}".format(num-nb_correct_pred))
 #     return signed_grad
 
 
+# x_0_arr = np.array([x_test[0] * num])
 # attacker = FastGradientMethod(classifier, eps=0.1)
 attacker = FastGradientSignMethod(classifier, eps=0.5)
-x_test_adv = attacker.generate(x_test[:num])
+# x_test_adv = attacker.generate(x_test[:num]) # non-targeted
+x_test_adv = attacker.generate(x_test[:num], x_test[0]) #targeted
 
 
 
 x_test_adv_pred = np.argmax(classifier.predict(x_test_adv), axis=1)
 nb_correct_adv_pred = np.sum(x_test_adv_pred == np.argmax(y_test[:num], axis=1))
 
+
 print(f"Adversarial test data (first {num} images):")
 print("Correctly classified: {}".format(nb_correct_adv_pred))
 print("Incorrectly classified: {}".format(num-nb_correct_adv_pred))
+nb_preb_as_tar = np.count_nonzero(x_test_adv_pred == np.argmax(y_test[:num], axis=1)[0])
+print(f"Classified as targeted label: {nb_preb_as_tar}\n\n")
+
+print(x_test_adv_pred)
+print(np.argmax(y_test[:num], axis=1))
+# print(x_test_pred)
+# print(x_test[0])
 
 # first_image = x_test[0]
 # first_image = np.array(first_image, dtype='float')
